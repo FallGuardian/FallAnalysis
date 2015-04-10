78f774a60fe86e339c723ada85924addee5786d4
youngcoma-server <ykhorizon.light@gmail.com>
2015-04-10 19:29:31 +0800

    read base_id from DB and write to .txt file

---
+++ sandbox/IdsWriteToFile.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+#!/usr/bin/python
+import numpy as np
+import csv
+from time import time 
+from datetime import datetime
+# Customized database function & login info. in database.py
+import database
+import dataManiplate
+db = database.initialize()
+cur = db.cursor()
+
+
+
+## We can write fast filter for user profile 
+fileName = '123.txt'
+fw = open('/home/youngcoma/Dropbox/newFallDetect/experiment/input_id/{}'.format(fileName) , 'w')
+allIds = database.getColDistinct(cur, 'final_base_id', 'base_id')
+outputStr = "";
+for base_id in allIds:
+	outputStr+=str(base_id[0])+","
+
+fw.write(outputStr)
