c11a8db5045847f74e08b844727449f1810cf0d7
youngcoma-server <ykhorizon.light@gmail.com>
2015-04-29 02:27:06 +0800

    add angle calculation, extract threshold comparing, general function

--- sandbox/dataManiplate.py
+++ sandbox/dataManiplate.py
@@ -1,54 +1,86 @@
 import math
 import numpy as np
+import sys
+
 
 def calVelocity( acc_x, acc_y, acc_z ):
 	return (math.sqrt(math.pow(acc_x,2)+math.pow(acc_y,2)+math.pow(acc_z,2))-9.81)
 
-# Questino: Do I need to model the function of angular displacement ?
-def calAngularDisplacement( gyroCumulation, sample_timing ):
-	return gyroCumulation*sample_timing
+def integrateAngle(acc1, acc2, gyro, paraDict, resultDict):
+	# angleGyro = rad2deg(gyro*paraDict['dt']) 
+	# # resultDict['angle'] += angleGyro
+	# angleAcc = rad2deg(math.atan2(acc1, acc2))
+	# # print angleAcc
+	# resultDict['angle'] = (1-paraDict['alpha'])*angleGyro+\
+	# paraDict['alpha']*angleAcc
+	
+	##
+	angleAcc = rad2deg(math.atan2(acc1, acc2))
+	resultDict['angle'] = (1-paraDict['alpha'])*(resultDict['angle'] + \
+	rad2deg(gyro * paraDict['dt'])) + (paraDict['alpha'])*(angleAcc)
+	
+	## Raw Version
+	# resultDict['angle'] += rad2deg(gyro*paraDict['dt'])
+	return resultDict['angle']
 
-def calGyroCumulation( gyroArr ):
-	gyroCumulation = 0
-	for constant,index in zip(range(400,0,-1),range(0,400)):
-		# print "i={}, j={}".format(constant,index)
-		gyroCumulation += gyroArr[index][0]*constant
 
-	return gyroCumulation
-def calVecolity():
-	return
 def calSVM( acc_x, acc_y, acc_z ):
 	return math.sqrt(math.pow(acc_x,2)+math.pow(acc_y,2)+math.pow(acc_z,2))
 
-def calTA( acc_x, acc_y, acc_z ):
+def calTA( acc_x, acc_y, acc_z):
 	if acc_x != 0 and acc_y != 0 and acc_z != 0:
-		return math.asin(acc_y/(math.sqrt(math.pow(acc_x,2)+math.pow(acc_y,2)+math.pow(acc_z,2))))*180/math.pi
+		return rad2deg(math.asin(acc_y/(math.sqrt(math.pow(acc_x,2)+math.pow(acc_y,2)+math.pow(acc_z,2)))))
 	else:
 		return 0
 
-def calAV( acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z ):
+def calAV( acc_x, acc_y, acc_z , gyro_x, gyro_y, gyro_z):
+	# Implem
 	return abs(acc_x*math.sin(gyro_x)+acc_y*math.sin(gyro_y)-acc_z*math.cos(acc_y)*math.cos(acc_z))
 
-def findOverThreshold(npDatas, AV, SVM):
-	overIds = []
-	for d in npDatas:
-		if calSVM(d['acc_x'], d['acc_y'], d['acc_z']) > SVM and calAV(d['acc_x'], d['acc_y'], d['acc_z'], d['gyro_x'], d['gyro_z'], d['acc_z']) > AV:
-			overIds.append(d['id'])
 
-	return overIds
+def getGlobalMax(datas):
+	return max(datas)
 
-def findOverThreshold2(npDatas, TA, SVM):
-	overIds = []
-	for d in npDatas:
-		if calTA(d['acc_x'], d['acc_y'], d['acc_z']) > TA and calSVM(d['acc_x'], d['acc_y'], d['acc_z']) > SVM:
-			overIds.append(d['id'])
+def getLocalMaxDiffence(Column, interval):
+	deltaMax = -sys.maxint
+	for i in range(0,400-interval):
+		localMax = max(Column[i:i+interval])
+		localMin = min(Column[i:i+interval])
+		deltaMax = max(localMax-localMin, deltaMax)
+	return deltaMax
 
-	return overIds
+def rad2deg(rad):
+	return rad*180.0/math.pi
 
-def findOverSVM(npDatas, SVM):
-	overIds = []
-	for d in npDatas:
-		if calSVM(d['acc_x'], d['acc_y'], d['acc_z']) > SVM:
-			overIds.append(d['id'])
+def deg2rad(deg):
+	return deg*math.pi/180.0
 
-	return overIds
+def findMaxCnt(cur, targerIds):
+	
+	maxCnt = 0
+	for _id in targerIds:
+		datas = list(database.getTableColsById(cur, targetTable, targetCols, _id))
+		npDatas = np.array(datas, dtype=zip(targetCols,targetTypes))
+
+		if npDatas[0]['label'] == 1:
+			max(maxCnt, len(dataManiplate.findOverThreshold(npDatas, AV, SVM)))
+	return maxCnt
+
+def thresholdBaseAlg(dataDict, _id, threasholdDict, paraDict, resultDict):
+	#@@ Cal SVM, TA @@##
+	
+	if dataDict[int(_id)][1] > threasholdDict['TA'] and	\
+	dataDict[int(_id)][0] > threasholdDict['SVM']:
+		## @@ non-fall check @@ ##
+		if dataDict[int(_id)][3] == 0:
+			resultDict['errorCntFP']+=1
+			resultDict['FPbaseIds'].append(str(_id))
+				
+			# if cnt > maxCnt and paraDict['maxCnt'] != -1:
+			# 	overMaxCntCnt+=1
+	else:
+		## @@ non-fall check @@ ##
+		if dataDict[int(_id)][3] == 1:
+			resultDict['errorCntTN']+=1
+			resultDict['TNbaseIds'].append(str(_id))
+
