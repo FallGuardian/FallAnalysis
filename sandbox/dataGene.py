a4a31400f5f2ae968e0839fbbeff30d45264791c
youngcoma-server <ykhorizon.light@gmail.com>
2015-05-12 16:54:07 +0800

    dataGene normalize and mode feature implementation

--- sandbox/dataGene.py
+++ sandbox/dataGene.py
@@ -3,32 +3,48 @@ import MySQLdb
 import csv
 import math
 import numpy as np
+import string
 from time import time 
 from datetime import datetime
 
 
-# Customized database function & login info. in database.py
+##
+##	Describes: This script calculates alg. method and generate data (caled_data) for testing accuracy
+##
+##	Usage: python dataGene.py idFile
+##	
+##	Input: file contained id (A) at /input_id/
+##
+##	Ouptut:  calculated data SVM,TA...etc (A_data) at /calculated_data/
+##
+
+##
+##	@@ Database initialization @@
+##
 import database
 import dataManiplate
-cur = database.initialize()
+db = database.initialize()
+cur = db.cursor()
 fileName = ''
-
+basePath = "/home/youngcoma/Dropbox/newFallDetect/experiment"
 
 ## @@ Script Input and Open File @@ ##
 if len(sys.argv) == 2:
 	if sys.argv[1] == '-h':
-		print '''Usage: python dataGene [ReadFileName.csv], [OutputFileName.csv]
-				ReadFileName.csv need to locate at folder /idSets/,
-				OutputFileName.csv will generate at folder /data/, default name is datetime.'''
+		print '''Usage: python dataGene [ReadFileName.csv]
+				ReadFileName.csv need to locate at folder /experiment/input_id/,'''
 		# Reading the file from idSets
 	else:
 		try:
 			fileName = sys.argv[1]
 			targerIds = []
-			with open("./idSets/{}".format(fileName), 'rb') as fp:
+			with open("{}/input_id/{}".format(basePath, fileName), 'rb') as fp:
 				reader = csv.reader(fp ,delimiter=',')
+
 				for row in reader:
-					targerIds.extend(row)
+					 for item in row:
+					 	if item:
+							targerIds.append(item)
 			pass
 		except IOError as e:
 			print "I/O error({0}): {1}".format(e.errno, e.strerror)
@@ -39,73 +55,95 @@ else:
 
 ## @@ Write File to /data/ @@ ##
 # t = time()
-# fileName = datetime.fromtimestamp(t).strftime('%Y-%m-%d_%H:%M:%S')
-fw = open('data/data_{}'.format(fileName) , 'w')
+# dt = datetime.fromtimestamp(t).strftime('%Y-%m-%d__%H_%M_%S')
+nameSeg = string.split(fileName,'.')
+outputName =  "{}_data.txt".format(nameSeg[0])
+
+fw = open("{}/calculated_data/{}".format(basePath, outputName) , 'w')
 
 ##
-## Output Format [base_id][maxSVM][TAdelta][Avdelta][label]
+## Output Format [base_id][SVM][TA][AV][label]
 ##
+## mode 1: Global Max
+## mode 2: Local(50 intervals) Max Difference 
+##	[SVM,TA,AV]
+mode=[1,1,1]
 
 # @@ Data Processing Setting @@ #
 brokenList = ""
 targetCols = ['base_id', 'id', 'acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z', 'label']
 targetTypes = ['i','i','f','f','f','f','f','f','i']
 
+## @@ para setting in AV ##
+dt = 6.0/400.0
+alpha = 0.02
+paraDict = {'alpha':alpha, 'dt':dt}
+
 ## @@ Data Process From DataBase @@ ##
 for _id in targerIds:
 	
-	print _id
-	datas = list(database.getTableColsById(cur, 'lab_primitive2', targetCols, _id))
 	
+	datas = list(database.getTableColsById(cur, 'final_primitive_total', targetCols, _id))
+	# print "{}: len {}".format(_id, len(datas))
 	
 	## @@ Data Clean / Making Up / Denoise @@ ##
 	if len(datas) != 400:
 		print '{} is broken'.format(_id)
 		last = len(datas) - 1
-		appendData = map(tuple, (
-				(0,0,datas[last][2],
-				datas[last][3],
-				datas[last][4],
-				datas[last][5],
-				datas[last][6],
-				datas[last][7],0)
+
+		appendData = map(tuple, ( (0,0,datas[last][2],datas[last][3],datas[last][4],\
+			datas[last][5],datas[last][6],datas[last][7],0)\
 			for i in range(len(datas), 400)))
+		
 		datas = datas + appendData
 
 	npDatas = np.array(datas, dtype=zip(targetCols,targetTypes))
-
+	
+	##
 	## @@ SVM calculate @@ ##
-	maxSVM = -sys.maxint
-	for d in npDatas:	
-		maxSVM = max(maxSVM, dataManiplate.calSVM(d['acc_x'], d['acc_y'], d['acc_z']))
+	##
+	SVMColumn = [dataManiplate.calSVM(d['acc_x'], d['acc_y'], d['acc_z']) for d in npDatas]
 
+	if mode[0] == 1:
+		SVM = dataManiplate.getGlobalMax(SVMColumn)
+	elif mode[0] == 2:
+		SVMinterval = 50	
+		SVM = dataManiplate.getLocalMaxDiffence(SVMColumn, SVMinterval)
+	
+	##
 	## @@ TA & TAdelta calculate @@ ##
-	TAinterval = 50
-	TAdeltaMax = -sys.maxint
+	##
 	TAColumn = [dataManiplate.calTA(d['acc_x'], d['acc_y'], d['acc_z']) for d in npDatas]
+	if mode[0] == 1:
+		TA = dataManiplate.getGlobalMax(TAColumn)
+	elif mode[0] == 2:
+		TAinterval = 50
+		TA = dataManiplate.getLocalMaxDiffence(TAColumn, TAinterval)
 
-	for i in range(0,350):
-		localMax = max(TAColumn[i:i+TAinterval])
-		localMin = min(TAColumn[i:i+TAinterval])
-		TAdeltaMax = max(localMax-localMin, TAdeltaMax)
-
+	##
 	## @@ AV & AVdelta calcalate @@ ##
-	AVinterval = 50
-	AVdeltaMax = -sys.maxint
-	AVColumn = [dataManiplate.calAV(d['acc_x'], d['acc_y'], d['acc_z'], d['gyro_x'], d['gyro_z'], d['acc_z'],) for d in npDatas]
-	
-	for i in range(0,350):
-		localMax = max(AVColumn[i:i+AVinterval])
-		localMin = min(AVColumn[i:i+AVinterval])
-		AVdeltaMax = max(localMax-localMin, AVdeltaMax)
-	
-	## @@ Write Out to File @@ ##		
-	outputStr ='{},{},{},{},{}\n'.format(_id, maxSVM, TAdeltaMax, AVdeltaMax, npDatas[0]['label'])
-	fw.write(outputStr)
-	# print outputStr
+	##
+	AVColumn = [dataManiplate.calAV(d['acc_x'], d['acc_y'], d['acc_z'], d['gyro_x'], d['gyro_y'], d['gyro_z']) for d in npDatas]
+	if mode[0] == 1:
+		AV = dataManiplate.getGlobalMax(AVColumn)
+	elif mode[0] == 2:
+		AVinterval = 50
+		AV = dataManiplate.getLocalMaxDiffence(AVColumn, AVinterval)
 
-print 'mission completed'
+	# dataManiplate.integrateAngle()
+
+	##
+	## @@ Write Out to File @@ ##		
+	##
+	# outputStr ='{},{},{},{},{}\n'.format(_id, SVM, TAdeltaMax, AVdeltaMax, npDatas[0]['label'])
+	outputStr ='{},{},{},{},{}\n'.format(_id, SVM, TA, AV, npDatas[0]['label'])
+	
+	# print outputStr
+	fw.write(outputStr)
+
+print 'dataGene completed, {}'.format(outputName) 
+
 fp.close()
 fw.close()
-# print 'Task completed!\n'
-# print 'broken List: {}'.format(brokenList)
+print 'Task completed!\n'
+print 'broken List: {}'.format(brokenList)
